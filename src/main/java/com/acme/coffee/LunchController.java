4a7755564be9e9ae3ad901e231c898809acb3068
lajos papp <lalyos@yahoo.com>
2012-10-01 13:03:08 +0200

    lunch controller added

---
+++ src/main/java/com/acme/coffee/LunchController.java
@@ -0,0 +1,13 @@
+package com.acme.coffee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LunchController {
+
+    @RequestMapping("/lunch")
+    public String lunch() {
+        return "lunch";
+    }
+}
