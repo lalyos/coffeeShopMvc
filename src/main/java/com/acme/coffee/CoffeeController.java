b8aaeb9c0cf1f7534c068396ca2046d718d33fe0
lajos papp <lalyos@yahoo.com>
2012-10-01 16:03:12 +0200

    handling binding error (string written into Price)

--- src/main/java/com/acme/coffee/CoffeeController.java
+++ src/main/java/com/acme/coffee/CoffeeController.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingErrorProcessor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,10 +44,14 @@ public class CoffeeController {
     }
     
     @RequestMapping(value="/save", method=RequestMethod.POST)
-    public String save(@ModelAttribute Coffee coffee, HttpSession session) {
-        
-        repository.add(coffee);
-        session.setAttribute("msg", "You have succesfully added: " + coffee.getName());
+    public String save(@ModelAttribute Coffee coffee, BindingResult bindingResult, HttpSession session) {
+
+        if (bindingResult.hasErrors()) {
+            return "coffee/edit";
+        } else {
+            repository.add(coffee);
+            session.setAttribute("msg", "You have succesfully added: " + coffee.getName());
+        }
        
         return "redirect:/coffee/list";
     }
